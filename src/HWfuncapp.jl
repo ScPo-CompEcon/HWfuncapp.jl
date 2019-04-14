e4baaed8bf0ba52a6c8cd8543c1ffe76f6f4deb6
takashissb <takashi.sasabe@sciencespo.fr>
2019-04-14 05:33:10 +0200

    Q1

--- src/HWfuncapp.jl
+++ src/HWfuncapp.jl
@@ -4,7 +4,7 @@ using FastGaussQuadrature # to get chebyshevnodes
 
 # you dont' have to use PyPlot. I did much of it in PyPlot, hence
 # you will see me often qualify code with `PyPlot.plot` or so.
-using PyPlot  
+using PyPlot
 import ApproXD: getBasis, BSpline
 using Distributions
 using ApproxFun
@@ -15,6 +15,21 @@ unitmap(x,lb,ub) = 2 .* (x .- lb) ./ (ub .- lb) .- 1	#[a,b] -> [-1,1]
 
 function q1(n)
 	f(x) = x .+ 2x.^2 - exp.(-x)
+	f(x) = x .+ 2x.^2 - exp.(-x)
+	deg = n-1
+	lb = -3
+	ub = 3
+	node = gausschebyshev(n)[1]
+	chebnode = 0.5 .* (lb .+ ub) .+ 0.5 .* (ub .- lb) .* node
+	y = map(f, chebnode)
+	basismatrix = [cos((n - i + 0.5) * (j - 1)* π / n) for i = 1:n, j = 1:n]
+	c = basismatrix \ y
+
+	n_new = 100
+	x_2 = range(lb, stop = ub, length = n_new)
+	base2 = [ChebyT(unitmap(x_2[i], lb, ub), j) for i in 1:n_new, j = 0:deg]
+	y2 = base2 * c
+	error = y2 - f(collect(-3:6/99:3))
 
 	# without using PyPlot, just erase the `PyPlot.` part
 	PyPlot.savefig(joinpath(dirname(@__FILE__),"..","q1.png"))
@@ -43,7 +58,7 @@ end
 
 # I found having those useful for q5
 mutable struct ChebyType
-	f::Function # fuction to approximate 
+	f::Function # fuction to approximate
 	nodes::Union{Vector,LinRange} # evaluation points
 	basis::Matrix # basis evaluated at nodes
 	coefs::Vector # estimated coefficients
@@ -79,7 +94,7 @@ function q5(deg=(5,9,15),lb=-1.0,ub=1.0)
 
 	runge(x) = 1.0 ./ (1 .+ 25 .* x.^2)
 
-	
+
 	PyPlot.savefig(joinpath(dirname(@__FILE__),"..","q5.png"))
 
 end
