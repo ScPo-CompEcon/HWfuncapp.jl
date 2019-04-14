4c9c6475b7d8eb47410335f4c97a50b97ae559d7
takashissb <takashi.sasabe@sciencespo.fr>
2019-04-15 00:01:23 +0200

    HW
    q3 test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 @testset "HWfuncapp.jl" begin
 	# test that q1 with 15 nodes has an error smaller than 1e-9
-    	@test q1(15)[:error] < 1e-9
+    @test q1(15)[:error] < 1e-9
     # test that the integral of function h [-10,0] ≈ 1.46039789878568
-
+	@test q3(10) ≈ 1.46039789878568
     @test false  # by default fail
 end
